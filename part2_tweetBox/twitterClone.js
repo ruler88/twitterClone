a3c586c32b6771c226f8a8f4c7056f51786a022b
ruler88 <helloworld0424@gmail.com>
2015-07-11 12:58:16 -0700

    Meteor Tutorial: twitterClone part2 - tweetBox

---
+++ part2_tweetBox/twitterClone.js
@@ -0,0 +1,46 @@
+Tweets = new Meteor.Collection("tweets");
+
+if (Meteor.isClient) {
+  Template.tweetBox.helpers({
+    charCount: function() {
+      return 140 - Session.get('numChars');
+    },
+
+    charClass: function() {
+      if (Session.get('numChars') > 140) {
+        return 'errCharCount';
+      } else {
+        return 'charCount';
+      }
+    },
+
+    disableButton: function() {
+      if (Session.get('numChars') <= 0 ||
+          Session.get('numChars') > 140) {
+        return 'disabled';
+      }
+    }
+  });
+
+  Template.tweetBox.events({
+    'input #tweetText': function(){
+      Session.set('numChars', $('#tweetText').val().length);
+    },
+
+    'click button': function() {
+      var tweet = $('#tweetText').val();
+      $('#tweetText').val("");
+      Tweets.insert({message: tweet});
+    }
+  });
+
+  Template.tweetBox.onRendered(function () {
+    Session.set('numChars', 0);
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
