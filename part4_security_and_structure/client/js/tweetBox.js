0252e11f2e2984ec33c4afdc568dbdfa17641f9d
ruler88 <helloworld0424@gmail.com>
2015-07-11 21:32:37 -0700

    Meteor Tutorial: twitterClone part4 - security & file structure

---
+++ part4_security_and_structure/client/js/tweetBox.js
@@ -0,0 +1,39 @@
+Template.tweetBox.helpers({
+  charCount: function() {
+    return 140 - Session.get('numChars');
+  },
+
+  charClass: function() {
+    if (Session.get('numChars') > 140) {
+      return 'errCharCount';
+    } else {
+      return 'charCount';
+    }
+  },
+
+  disableButton: function() {
+    if (Session.get('numChars') <= 0 ||
+        Session.get('numChars') > 140 ||
+        !Meteor.user()) {
+      return 'disabled';
+    }
+  }
+});
+
+Template.tweetBox.events({
+  'input #tweetText': function(){
+    Session.set('numChars', $('#tweetText').val().length);
+  },
+
+  'click button': function() {
+    var tweet = $('#tweetText').val();
+    $('#tweetText').val("");
+    if (Meteor.user()) {
+      Tweets.insert({message: tweet, user: Meteor.user().username});
+    }
+  }
+});
+
+Template.tweetBox.onRendered(function () {
+  Session.set('numChars', 0);
+});
